3c9ccdb0cdd779017df7722f29adc3cd918e269f
jack <khoicaominh.mmt@gmail.com>
2019-11-20 12:44:13 +0700

    add firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAOJTSxYAOBtSodxJ30ktnb34n3OPxxjUM',
+    authDomain: 'ngrx-lab.firebaseapp.com',
+    databaseURL: 'https://ngrx-lab.firebaseio.com',
+    projectId: 'ngrx-lab',
+    storageBucket: 'ngrx-lab.appspot.com',
+    messagingSenderId: '308969038006',
+    appId: '1:308969038006:web:b5edb98a77c325ef5b4617',
+    measurementId: 'G-CCL80H5ES7'
+}
 };
